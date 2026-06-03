eef390e99a1f389d5d0c57dff94879843be92d8b
castillios <maleahcast@gmail.com>
2026-06-02 17:24:11 -0700

    update bool for permissions

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -67,7 +67,8 @@ async function main({ g, c }, { shouldPost, issueNum }) {
     assignee = await getLatestAssignee();
 
     // Check if developer is allowed to work on this issue
-    const isAdminOrMerge = await memberOfAdminOrMergeTeam();
+    // const isAdminOrMerge = await memberOfAdminOrMergeTeam();
+    const isAdminOrMerge = false; // TESTING 8634-5
     const isAssignedToAnotherIssue = await assignedToAnotherIssue();
 
     // Check if developer is allowed to work on complexity level of the issue
